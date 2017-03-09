3f542c1bf04136394d8581968c1b276aeca7f824
licors <licors214@gmail.com>
2017-03-09 15:37:28 +0900

    웹서버 요청 기준  get,post 기준으로 구분하던 것을 url 경로에 따라 처리되도록 기준 변경 post 방식으로 회원가입 데이터 처리 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -31,7 +32,9 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream(); 
+        		OutputStream out = connection.getOutputStream()) {
+        	
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
     		BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
     		String line = br.readLine();
@@ -42,41 +45,59 @@ public class RequestHandler extends Thread {
     		}
     		
     		String[] tokens = line.split(" ");
-    		String requestPath = tokens[1];;
-    		String params = "";
-    		
-    		//get 방식으로 param 넘어온 경우
-    		if(tokens[0].equals("GET")) {
-	    		String url = tokens[1];
-	    		int index = url.indexOf("?");
-	    		if(index != -1) {
-		    		requestPath = url.substring(0, index);
-		    		params = url.substring(index+1);
-		    		Map<String, String> userData = HttpRequestUtils.parseQueryString(params);
-		    		User user = new User(userData.get("userId"), userData.get("password"), userData.get("name"), userData.get("email"));
-		    		log.debug("User class data {}",user);
-		    		DataBase.addUser(user);
-	    		}
-    		} else if(tokens[0].equals("POST")) {
-    			
-    		}
+    		String url = tokens[1];;
+    		String queryString = "";
+    		int contentLength = -1;
     		
     		while(!line.equals("")) {
     			line = br.readLine();
     			log.debug("header : {}",line);
+    			if(line.contains("Content-Length")) {
+    				contentLength = getHeaderValue(line);
+    			}
+    		}
+
+    		// 1. url 검사
+    		if(url.startsWith("/user/create")) {
+    			
+    			int index = url.indexOf("?");
+    			// 2-1. get방식인 경우
+	    		if(index != -1) {
+		    		queryString = url.substring(index+1);
+		    		//DataBase.addUser(user);
+	    		}
+	    		
+	    		// 2-2. post 방식인 경우
+	    		if(contentLength != -1) {
+	    			queryString = IOUtils.readData(br, contentLength);
+	    		}
+	    		
+	    		Map<String, String> userData = HttpRequestUtils.parseQueryString(queryString);
+	    		User user = new User(userData.get("userId"), userData.get("password"), 
+	    								userData.get("name"), userData.get("email"));
+	    		log.debug("User class data {}",user);
+    		}
+    		else {
+    			//응답 데이터 만들어서 응답함
+                DataOutputStream dos = new DataOutputStream(out);
+                //byte[] body = "Hello WorldTest".getBytes();    //서버 동작 테스트용
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
     		}
     		
-    		//응답 데이터 만들어서 응답함
-            DataOutputStream dos = new DataOutputStream(out);
-            //byte[] body = "Hello WorldTest".getBytes();    //서버 동작 테스트용
-            byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+    		
+    		
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
+    private int getHeaderValue(String line) {
+    	String[] headerData = line.split(":");
+    	return Integer.parseInt(headerData[1].trim());
+    }
+    
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
