72d6fd0892d4bb0538a2e6349b6dd70aa9b59522
licors <licors214@gmail.com>
2017-03-15 00:55:05 +0900

    request header 파싱하는 클래스 만들기 위해 테스트코드 작성

---
+++ src/test/java/webserver/HttpRequestTest.java
@@ -0,0 +1,29 @@
+package webserver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpRequestTest {
+	private static final Logger log = LoggerFactory.getLogger(HttpRequestTest.class);
+	private final String testDirectoryPath = "./src/test/resource/";
+	
+	@Test
+	public void request_Get() throws Exception{
+		InputStream in = new FileInputStream(new File(testDirectoryPath));
+		HttpRequest request = new HttpRequest(in);
+		
+		assertEquals("GET",request.getMethod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("aaa", request.getParameter("userId"));
+	}
+}
